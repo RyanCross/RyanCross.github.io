b5be48e20cf34ab57296814cf5b4fd8c67b76198
RyanCross <StudioPod.RyanCross@outlook.com>
2020-05-06 21:12:24 -0500

    Moving web content away from html to module, turns out modules need a local server like node to run

---
+++ src/data/me.js
@@ -0,0 +1,23 @@
+module.exports.me = {
+  "basics": {
+    "firstName":"Ryan",
+    "lastName": "Cross",
+    "age": 26,
+    "sex": "Male",
+    "location": {
+      "country": "United States",
+      "state": "Missouri",
+      "city": "Kansas City"
+    },
+    "occupation": "Senior Software Engineer",
+    "employer": "Cerner Corporation",
+    "label": "Software Engineer x Game Developer"
+  },
+  "about": {
+    "paragraphOne": "Welcome. I'm Ryan and I'm a senior software engineer by day. I've spent the last few years in the HealthCare IT space @ Cerner; Building robust APIs and web applications for a range of healthcare concepts. I've been fortunate to work with a team of extremely talented engineers from which I have learned a great deal. Creating services that helps health organizations across the globe share important patient information quickly and with better confidence and trust.",
+    "paragraphTwo": "In my non-working hours, my productive efforts shift towards my long-term dream of building games. Working in C# and the Unity engine building prototypes or reverse engineer a game mechanic I find intrigueing. You'll also frequently find me hacking away on absurd ideas at the nearest available Game Jam. Creating that tower defense dating sim you never knew you wanted.",
+    "paragraphThree": "Occasionally I can be found visiting the outside world for huu-man interaction. Sometimes in the form of hiking and skiing when the weather permits; but more often you'll find me at the local board game bar, an up-and-coming venue trend I am eternally grateful for.",
+    "paragraphFour": "If your interested in learning more about my industry work, head on over to my resume. For my passion projects, check out the projects page."
+  },
+  "copyright": "Contents of this site are © Copyright 2019-2020 Ryan Cross. All rights reserved. <a href='https://github.com/RyanCross/ryancross.github.io' title='ryancross.github.io' target='_blank'>Source </a> code is available under the MIT license."
+}
